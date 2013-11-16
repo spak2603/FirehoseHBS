25ad736422bc19d3162f5a068b617c41a49c58ff
Sylvia Pak <sylviapak@gmail.com>
2013-11-16 17:23:36 -0500

    Added new and create o the pics controller

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -3,4 +3,26 @@ class PicsController < ApplicationController
 		@pics = Pic.all
 		#@pics is a variable.... where you access it is in the view.... capital P is important
 	end
+
+	def new
+		@pic = Pic.new
+	end
+ 
+ # for the create button / save button 
+	 def create
+	 		@pic = Pic.create(pic_params)
+	 		redirect_to pics_path 
+	 end
+
+
+
+	 private
+
+# creating a function or a method 
+	 def  pic_params
+	 		params.require(:pic).permit(:lesson, :emotion, :learned)
+	 		#can specify what things you want htem to be abel to change
+	 		# grab all the things from this pic thing.
+	 end
+
 end
